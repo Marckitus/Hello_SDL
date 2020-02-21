5a703ba89d557ebdd1e9e339e70687c9113808ee
Marc Pavon Llop <60131792+Marckitus@users.noreply.github.com>
2020-02-21 09:58:52 +0100

    Window Delay Display

--- Awesome_Game/Main.cpp
+++ Awesome_Game/Main.cpp
@@ -1,9 +1,35 @@
 #include "SDL/include/SDL.h"
 #pragma comment( lib, "SDL/libx86/SDL2.lib" )
 #pragma comment( lib, "SDL/libx86/SDL2main.lib" )
+#include <iostream>
 
 
+int main(int argc, char** argv) {
+	std::cout << "Have " << argc << " arguments:" << std::endl;
+	for (int i = 0; i < argc; ++i) {
+		std::cout << argv[i] << std::endl;
+	}
+	//Inicia todos los sistemas, y da mensaje de error si no van..	
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
 
-int main(void) {
+	SDL_Window* win = SDL_CreateWindow("Hello World!", 100, 100, 1920, 1080, SDL_WINDOW_SHOWN);
+	if (win == nullptr) {
+		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (ren == nullptr) {
+		SDL_DestroyWindow(win);
+		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+	SDL_Delay(99000);
+
 	return 0;
 }
